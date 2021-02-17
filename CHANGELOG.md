87fa09fc309c64bf54462445518b4bab7aabc456
Phil Boyd <phil.boyd@prb-services-llc.com>
2021-02-17 09:50:17 -0500

    build: update build process

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 1.0.0-alpha.1 (2021-02-17)
+
+
+### Features
+
+* **section-10:** init section-10 ([b089867](https://github.com/MHacker9404/nx-repo/commit/b089867306cb01617bf9f37d681500aae4652938))
+* **udemy-typescript-tcdg:** section-09 complete ([fd87b1c](https://github.com/MHacker9404/nx-repo/commit/fd87b1c1d72aa2e7bd3caadef40a1db5e80cbd94))
+* **udemy-typescript-tcdg:** WIP ([c635418](https://github.com/MHacker9404/nx-repo/commit/c635418f49127045db11dd7ce93de74b75d5176b))
+* **udemy-typescript-tcdg:** WIP - marching on ([3b23cf5](https://github.com/MHacker9404/nx-repo/commit/3b23cf5d0d2c7e513b3a4540735765b575316338))
--- release.config.js
+++ release.config.js
@@ -19,11 +19,24 @@ module.exports = {
             prerelease: 'alpha',
         },
     ],
-    prepare: ['@semantic-release/changelog'],
+    prepare: [
+        '@semantic-release/changelog',
+        {
+            path: '@semantic-release/git',
+            assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: false,
+            },
+        ],
         '@semantic-release/github',
         '@semantic-release/git',
     ],
